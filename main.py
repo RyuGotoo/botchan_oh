4227c566088e64023f08a10bc4a6c24df8077f23
RyuGotoo <ryu.gotoo@gmail.com>
2020-02-23 19:07:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import collections
+from janome.tokenizer import Tokenizer
+import csv
+
+path = 'text.txt'
+# path = 'botchan.txt'
 
 
 class Blog(object):
@@ -6,10 +11,15 @@ class Blog(object):
         self.pos_num = 0
         self.pos_counts = {}
 
+    def get_pos_rates(self):
+        pos_rates = {}
+        for pos, count in self.pos_counts.items():
+            pos_rates[pos] = count / self.pos_num
+        return pos_rates
+
 
 def get_text():
-    with open('botchan.txt') as f:
-    # with open('text.txt') as f:
+    with open(path) as f:
         text = f.read()
     return text
 
@@ -18,22 +28,21 @@ def create_blogs(text):
     blog = Blog(text)
 
     # 形態素解析
-    from janome.tokenizer import Tokenizer
     pos_list = []
     for token in Tokenizer().tokenize(text):
         pos = token.part_of_speech.split(',')[0]
-        if pos != '記号':
+        base_pos = ('名詞', '助詞', '動詞', '助動詞', '副詞', '形容詞', '連体詞', '接頭詞', '接続詞', '感動詞')
+        if pos in base_pos:
             pos_list.append(pos)
     blog.pos_num = len(pos_list)
 
     # 品詞 (pos) カウント
-    pos_counts = collections.Counter(pos_list)
-    blog.pos_counts = pos_counts
+    blog.pos_counts = collections.Counter(pos_list)
+    blog.pos_counts = dict(sorted(blog.pos_counts.items(), key=lambda x: -x[1]))
     return blog
 
 
 def show_result(blog):
-    blog.pos_counts = dict(sorted(blog.pos_counts.items(), key=lambda x: -x[1]))
     print(get_text())
     print()
     print('総単語数 :', blog.pos_num)
@@ -43,14 +52,13 @@ def show_result(blog):
 
 
 def export_csv(blog):
-    pos_rates = {}
-    for pos, count in blog.pos_counts.items():
-        pos_rates[pos] = count / blog.pos_num
-
-    import csv
-    pos_rates['id'] = 'botchan'
     with open('results.csv', 'w') as f:
-        writer = csv.DictWriter(f, ['id', '名詞', '助詞', '動詞', '助動詞', '副詞', '形容詞', '連体詞', '接頭詞', '接続詞', '感動詞', 'フィラー', 'その他'])
+        base_pos = ['名詞', '助詞', '動詞', '助動詞', '副詞', '形容詞', '連体詞', '接頭詞', '接続詞', '感動詞']
+        header = ['id'] + base_pos
+        writer = csv.DictWriter(f, header)
+
+        pos_rates = blog.get_pos_rates()
+        pos_rates['id'] = 'botchan'
         writer.writeheader()
         writer.writerow(pos_rates)
 
@@ -59,7 +67,7 @@ def main():
     text = get_text()
     blog = create_blogs(text)
     show_result(blog)
-    export_csv(blog)
+    # export_csv(blog)
 
 
 if __name__ == '__main__':
